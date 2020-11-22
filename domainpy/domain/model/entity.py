f172bcca61cefc8e6a54cd69980309260a17e1e7
Brian Estrada <brianseg014@gmail.com>
2020-11-22 23:21:06 +0000

    add apply to domain entity

--- domainpy/domain/model/entity.py
+++ domainpy/domain/model/entity.py
@@ -1,6 +1,12 @@
 
 class DomainEntity:
     
+    def __init__(self, aggregate):
+        self.__aggregate__ = aggregate
+        
+    def __apply__(self, event):
+        self.__aggregate__.__apply__(event)
+    
     def __route__(self, event):
         self.mutate(event)
         
