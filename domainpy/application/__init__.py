29ffe69cf94d4279dccf1fb037e69009bfdeb550
Brian <brianseg014@gmail.com>
2021-02-24 21:17:12 -0600

    add registry

--- domainpy/application/__init__.py
+++ domainpy/application/__init__.py
@@ -1,10 +1,12 @@
 
-from domainpy.application.bus import Bus, Commutator
-from domainpy.application.service import ApplicationService
-from domainpy.application.command import ApplicationCommand
-from domainpy.application.query import ApplicationQuery
-from domainpy.application.projection import Projection
-from domainpy.application.handler import handler
-from domainpy.application.projector import projector
-from domainpy.application.mappers.commandmapper import CommandMapper
-from domainpy.application.mappers.eventmapper import EventMapper
+from .mappers.commandmapper import CommandMapper
+from .mappers.eventmapper import EventMapper
+
+from .bus import Bus, Commutator
+from .command import ApplicationCommand
+from .handler import handler
+from .projection import Projection
+from .projector import projector
+from .query import ApplicationQuery
+from .registry import Registry
+from .service import ApplicationService
