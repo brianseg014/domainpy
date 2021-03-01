6865030843efc297922acfcc203e1afaf5417b82
Brian <brianseg014@gmail.com>
2021-03-01 16:01:24 -0600

    add specification

--- domainpy/domain/__init__.py
+++ domainpy/domain/__init__.py
@@ -5,4 +5,5 @@ from domainpy.domain.model.event import DomainEvent
 from domainpy.domain.model.value_object import ValueObject, Identity
 from domainpy.domain.repository import Repository
 from domainpy.domain.service import DomainService
+from domainpy.domain.specification import Specification
 from domainpy.domain.model.mutator import mutator
