49b207650d066fba24fddc7ac21d8fd12c346556
Brian <brianseg014@gmail.com>
2021-03-18 18:16:17 -0600

    remove view import

--- domainpy/domain/__init__.py
+++ domainpy/domain/__init__.py
@@ -4,7 +4,6 @@ from domainpy.domain.model.entity import DomainEntity
 from domainpy.domain.model.event import DomainEvent
 from domainpy.domain.model.value_object import ValueObject, Identity
 from domainpy.domain.model.specification import Specification
-from domainpy.domain.model.view import View
 from domainpy.domain.model.mutator import mutator
 
 from domainpy.domain.repository import Repository
