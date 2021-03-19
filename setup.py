bdd2d14d6cafc0d19953a1d302c4084e70c63e78
Brian <brianseg014@gmail.com>
2021-03-18 19:14:31 -0600

    fix omitted inits

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='domainpy',
-    version='0.1.1',
+    version='0.1.2',
     description='A library for DDD, ES, CQRS, TDD + BDD and microservices',
     author='Brian Estrada',
     author_email='brianseg014@gmail.com',
     packages=find_packages(),
     license='MIT',
     url='https://github.com/mymamachef/domainpy',
-    download_url='https://github.com/mymamachef/domainpy/archive/v0.1.1.tar.gz',
+    download_url='https://github.com/mymamachef/domainpy/archive/v0.1.2.tar.gz',
     keywords=['ddd', 'event sourcing', 'CQRS'],
     install_requires=['boto3']
 )
