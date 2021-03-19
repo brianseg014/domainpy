590bbdb78f959f9e82d1d8085929b6467add8793
Brian <brianseg014@gmail.com>
2021-03-18 18:46:54 -0600

    mark as version 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='domainpy',
-    version='0.0.1',
+    version='0.1.0',
     description='A library for DDD, ES, CQRS, TDD + BDD and microservices',
     author='Brian Estrada',
     author_email='brianseg014@gmail.com',
