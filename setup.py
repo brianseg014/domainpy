d0f3cee265dc2bef75f426c022a6a80d96dcec02
Brian <brianseg014@gmail.com>
2021-07-12 16:32:12 -0600

    fix version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(),
     license='MIT',
     url='https://github.com/mymamachef/domainpy',
-    download_url='https://github.com/mymamachef/domainpy/archive/v0.1.22.tar.gz',
+    download_url='https://github.com/mymamachef/domainpy/archive/v0.2.0.tar.gz',
     keywords=['ddd', 'event sourcing', 'CQRS'],
     install_requires=['boto3==1.17.109']
 )
