3dd63619e1e6e08765f8fd1216cc3f094a443dde
Brian <brianseg014@gmail.com>
2021-07-20 16:28:48 -0600

    relocate domain error into model

--- domainpy/domain/__init__.py
+++ domainpy/domain/__init__.py
@@ -1,4 +1,3 @@
-
 from .repository import IRepository
 from .service import IDomainService
 
--- domainpy/domain/model/__init__.py
+++ domainpy/domain/model/__init__.py
@@ -13,5 +13,5 @@ __all__ = [
     "Specification",
     "ValueObject",
     "Identity",
-    "DomainError"
+    "DomainError",
 ]
