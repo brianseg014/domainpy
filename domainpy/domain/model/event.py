549d18a525a6bfa81aecc4bc15781e9b99a5805b
Brian <brianseg014@gmail.com>
2021-07-24 23:06:48 -0600

    simplify domain event eq

--- domainpy/domain/model/event.py
+++ domainpy/domain/model/event.py
@@ -9,15 +9,9 @@ class DomainEvent(SystemData, Traceable):
     __version__: int
 
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, DomainEvent):
+        if not isinstance(other, self.__class__):
             return False
-
-        if other is None:
-            return False
-
-        if self.__class__ != other.__class__:
-            return False
-
+        
         return (
             self.__stream_id__ == other.__stream_id__
             and self.__number__ == other.__number__
