19486d1173692a123302cf3e1786fde524d40d85
Brian <brianseg014@gmail.com>
2021-07-30 23:15:51 -0600

    add eq to domain event

--- domainpy/domain/model/event.py
+++ domainpy/domain/model/event.py
@@ -8,3 +8,12 @@ class DomainEvent(SystemData, Traceable, Contextualized):
     __number__: int
     __timestamp__: float
     __version__: int = 1
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, self.__class__):
+            return False
+
+        return (
+            self.__stream_id__ == o.__stream_id__
+            and self.__number__ == o.__number__
+        )
