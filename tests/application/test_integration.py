b493be5b6946c5fa0bc4a1382a5b3ac7649d229f
Brian Estrada <brianseg014@gmail.com>
2021-08-15 08:24:03 -0600

    increase test coverage

---
+++ tests/application/test_integration.py
@@ -0,0 +1,29 @@
+
+from domainpy.utils.contextualized import Contextualized
+from domainpy.utils.traceable import Traceable
+from domainpy.application.integration import IntegrationEvent
+
+
+def test_stamp_with_args():
+    m = IntegrationEvent.stamp(
+        trace_id='tid',
+        context='context'
+    )(
+        __timestamp__=0.0,
+        __resolve__='success',
+        __error__=None,
+    )
+    assert m.__trace_id__ == 'tid'
+    assert m.__context__ == 'context'
+    
+def test_stamp_without_args():
+    Traceable.set_default_trace_id('tid')
+    Contextualized.__context__ = 'context'
+    
+    m = IntegrationEvent.stamp()(
+        __timestamp__=0.0,
+        __resolve__='success',
+        __error__=None,
+    )
+    assert m.__trace_id__ == 'tid'
+    assert m.__context__ == 'context'
