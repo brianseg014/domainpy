bd8683f1215666b3cb2de58c799dfd616cc9a48b
Brian Estrada <brianseg014@gmail.com>
2021-09-11 13:18:59 -0600

    enable mapper to index with context

--- domainpy/infrastructure/mappers.py
+++ domainpy/infrastructure/mappers.py
@@ -1,5 +1,6 @@
 import typing
 
+from domainpy.exceptions import DefinitionError
 from domainpy.infrastructure.transcoder import Transcoder
 from domainpy.typing.infrastructure import InfrastructureMessage
 from domainpy.typing.infrastructure import InfrastructureRecord
@@ -13,11 +14,31 @@ class Mapper:
     def __init__(self, transcoder: Transcoder) -> None:
         self.transcoder = transcoder
 
-        self.map: typing.Dict[str, typing.Any] = {}
+        self._map: typing.Dict[str, typing.Any] = {}
 
     def register(self, cls):
-        self.map[cls.__name__] = cls
+        context = getattr(cls, '__context__', 'default')
+        topic = cls.__name__
+
+        default = f'default:{topic}'
+        contexted = f'{context}:{topic}'
+        if contexted in self._map:
+            if default == contexted:
+                raise DefinitionError(f'Ambiguous name. Consider add __context__ field for disambiguation.')
+
+        self._map[default] = cls
+        self._map[contexted] = cls
         return cls
+
+    def get(self, topic: str, context: str = 'default') -> type:
+        default = f'default:{topic}'
+        contexted = f'{context}:{topic}'
+        if default in self._map:
+            return self._map[default]
+        elif contexted in self._map:
+            return self._map[contexted]
+
+        return None
 
     def serialize(
         self, message: InfrastructureMessage
@@ -29,11 +50,13 @@ class Mapper:
     def deserialize(
         self, record: InfrastructureRecord
     ) -> InfrastructureMessage:
-        try:
-            message_type = self.map[record.topic]
-        except KeyError as error:
+        context = getattr(record, 'context', 'default')
+        topic = record.topic
+
+        message_type = self.get(topic, context)
+        if message_type is None:
             raise MessageTypeNotFoundError(
-                f"unable to find type {record.topic}"
-            ) from error
+                f"unable to find type {topic}"
+            )
 
         return self.transcoder.deserialize(record, message_type)
